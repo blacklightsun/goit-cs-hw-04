6ae06f7a3fd09b3541172dd7b42f9025f5c656ba
blacklightsun <blacklightsun@gmail.com>
2024-07-12 09:44:18 +0300

    4/4

--- task2_ProcessPoolExecutor.py
+++ task2_ProcessPoolExecutor.py
@@ -1,30 +1,23 @@
 from pathlib import Path
 import time
 import concurrent.futures
-from multiprocessing import set_start_method
 
-def wrapper(result_dict):
-    # print('globals', globals().keys())
-    # print('locals', locals().keys())
-    def search_func(file):
-        # функція для запуска в процесах
-        res_list = []
-        with open(file, 'r') as f:
-            string = f.read().lower()
-        for key in result_dict.keys():
-            if string.find(key) != -1:
-                res_list.append((key, file))
-        print(f'{file}\'s processing process is finished')
-        return res_list # повертаємо список таплів
-    return search_func
+def search_func(file):
+    # функція для запуска в процесах
+    res_list = []
+    with open(file, 'r') as f:
+        string = f.read().lower()
+    for key in result_dict.keys():
+        if string.find(key) != -1:
+            res_list.append((key, file))
+    print(f'{file}\'s processing process is finished')
+    return res_list # повертаємо список таплів слово-файл
 
 if __name__ == '__main__':
-    # set_start_method('forkserver')
 
     # створення списку файлів з вказаної папки
     search_path = Path(input('Enter search path: '))
     file_list = [str(x) for x in search_path.iterdir() if x.is_file()]
-
 
     # створення словника з ключовими словами пошуку
     result_dict = {}
@@ -33,18 +26,9 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
-    # створення замикання для вказання цільового словника result_dict
-    search_func_in_result_dict = wrapper(result_dict)
-    print('globals', globals().keys())
-    print('locals', locals().keys())
-    print(search_func_in_result_dict)
-    print(type(search_func_in_result_dict))
-    print(dir(search_func_in_result_dict))
-    print(search_func_in_result_dict(file_list[1]))
-
     # запуск процесів (по замовчанню запускається стільки процесів, скільки ядер)
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        ll = executor.map(search_func_in_result_dict, file_list)
+        ll = executor.map(search_func, file_list)
 
     # об'єднуємо список результатів від кожного процеса в єдиний список
     list_of_pair = []
